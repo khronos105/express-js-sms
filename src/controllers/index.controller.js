26c6d7c643d9455dc33ac4892d8e09bc251034b0
Roma Bilibov <romabilibov@gmail.com>
2022-12-27 21:31:18 +0100

    Cleaning routes file

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,30 @@
+const config = require('../config')
+const { sendMessage } = require('../twilio/send-sms')
+const SMS = require('../models/sms')
+
+const indexController = async (res, req) => {
+    const messages = await SMS.find().lean()
+    messages.forEach(m => console.log(m.body))
+    res.render('index', { messages })
+}
+
+const postMessage = async (req, res) => {
+    const {phone, message} = req.body;
+
+    if(!phone && !message) return res.json('Missing message or phone');
+
+    const response = await sendMessage(phone, message)
+
+    await SMS.create({
+        body: req.body.message, 
+        to: req.body.phone,
+        from: config.twilio.from
+    })
+    
+    res.redirect('/')
+}
+
+module.exports = {
+    indexController,
+    postMessage
+}
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,29 +1,10 @@
 const { Router } = require('express')
 const router = Router()
-const config = require('../config')
-const {sendMessage} = require('../twilio/send-sms')
-const SMS = require('../models/sms')
 
-router.get('/', async (req, res) => {
-    const messages = await SMS.find().lean()
-    messages.forEach(m => console.log(m.body))
-    res.render('index', { messages })
-})
+const { indexController, postMessage } = require('../controllers/index.controller')
 
-router.post('/send-sms', async (req, res) => {
-    const {phone, message} = req.body;
+router.get('/', indexController)
 
-    if(!phone && !message) return res.json('Missing message or phone');
-
-    const response = await sendMessage(phone, message)
-
-    await SMS.create({
-        body: req.body.message, 
-        to: req.body.phone,
-        from: config.twilio.from
-    })
-    
-    res.redirect('/')
-})
+router.post('/send-sms', postMessage)
 
 module.exports = router;
